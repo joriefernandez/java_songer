cb6c4ef20c4e4a752ea6ddc8a6d5713ed491fa01
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-12 19:27:11 -0700

    Added demo

--- src/main/java/com/fern/j/project/firstWebApp/NotRestController.java
+++ src/main/java/com/fern/j/project/firstWebApp/NotRestController.java
@@ -1,13 +1,16 @@
 package com.fern.j.project.firstWebApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NotRestController {
 
     @GetMapping("/notresthello")
-    public String getHello(){
+    public String getHello(Model m){
+        String[] allTheNames = new String[]{"Michelle", "Evan", "reina"};
+        m.addAttribute("names", allTheNames);
         return "hello";
     }
 
